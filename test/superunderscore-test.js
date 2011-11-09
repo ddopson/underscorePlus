a3fc7ca828ccc769ebc2b92babf641e545b8af34
Dave Dopson <ddopson@zynga.com>
2011-11-08 16:30:00 -0800

    initial commit

---
+++ test/superunderscore-test.js
@@ -0,0 +1,18 @@
+var _ = require('underscore');
+var vows = require('vows');
+var assert = require('assert');
+var superunderscore = require('../lib/superunderscore');
+
+vows.describe('superunderscore')
+.addBatch({
+  'underscore': {
+    '_.map is a functionn': function() {
+      assert.isFunction(_.map);
+    }
+    ,'_.sprintf is a functionn': function() {
+      assert.isFunction(_.sprintf);
+    }
+  }
+})
+.export(module);
+
